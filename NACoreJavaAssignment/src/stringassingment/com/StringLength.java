f437633ce4c5d237034a37740a458aa15ff51459
mpriyada <pmonisha018@gmail.com>
2023-07-25 00:02:27 +0530

    String Length Added

---
+++ NACoreJavaAssignment/src/stringassingment/com/StringLength.java
@@ -0,0 +1,12 @@
+package stringassingment.com;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello World";
+		int n = str.length();
+		System.out.println("Length of the given string is: "+n);
+	}
+
+}
